5865df43c8722b3ec9620e464fe902c187e0a6c5
Lindsay Hammon <lindsay.work.mail@gmail.com>
2015-01-14 14:51:24 -0700

    sets strong params in the controllers

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,16 +1,9 @@
 class LocationsController < ApplicationController
   def show
-    @location = current_user.locations.find_by(:id => params[:location_id])
-    if @location
-      # done, go to the show page
-    else
-      flash[:warning] = 'Invalid Location'
-      redirect_to current_user
-    end
+    @locations = current_user.locations.find_by(:id => params[:location_id])
   end
 
   def index
-    @user = current_user
     @location = @user.locations
   end
 
@@ -22,4 +15,28 @@ class LocationsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def set_user 
+       @user = current_user
+    end 
+
+
+    def set_item
+      @item =  Item.find_by(id: params[:id], user_id: current_user)    
+    end
+
+    def set_location
+     @locations = current_user.locations.find_by(:id => params[:location_id])
+    end
+
+    def item_params
+      params.require(:item).permit(:title, :description, :is_out)
+    end
+    
+    def location_params
+      params.require(:loacation).permit(:title, :description)
+    end
+
 end

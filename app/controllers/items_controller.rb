fb51c9a93226faf5c0b24c6068cb4f7e70f024a7
Lindsay Hammon <lindsay.work.mail@gmail.com>
2015-01-16 11:27:37 -0700

    adds basic CRUD

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,26 +2,37 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_task, only: [:show, :edit, :update, :destroy, :toggle_is_completed]
   before_action :set_user
-
+  before_action :set_location
   def new
-    
+    @item = @user.items.build
   end
 
   def create
-    
+    @item = @location.item.build(item_params)
+    if @task.save
+      flash[:success] = "Item #{@item.name} added."
+      redirect_to user_task_path(@user, @task)
+    else
+      render 'new'
+    end
   end
 
-  def edit
-    
-  end
+  def update
+    if @task.update_attributes(item_params)
+      redirect_to user_tasks_path
+    else
+      flash[:danger] = "Item creation faild"
+      render 'edit'
+    end
 
   def update
     
   end
 
   def destroy
-    
-  end
+    @task.delete
+    redirect_to user_tasks_path
+    end
 
 
 
@@ -46,7 +57,7 @@ class ItemsController < ApplicationController
     end
 
     def item_params
-      params.require(:itm).permit(:title, :description, :is_out)
+      params.require(:item).permit(:name, :description, :is_out, :date_due)
     end
 
 end

59fa7aeef7d64dc13b46f5aa8c0bab0c8b79ecfa
Lindsay Hammon <lindsay.work.mail@gmail.com>
2015-01-15 19:26:36 -0700

    updates the git ignore

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,41 @@
 class ItemsController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :toggle_is_completed]
+  before_action :set_user
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+
+
+
+  private 
   def show
     @items =  Item.find_by(user_id: current_user)
   end
 
   private
+
+    def set_user
+      @user = current_user
+    end
 
     def set_item
       @item =  Item.find_by(id: params[:id], user_id: current_user)    

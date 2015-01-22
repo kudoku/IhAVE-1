6ad8c3367b8c21f610d3ce9d6f7c510e02561e37
JPark <park.joonw@gmail.com>
2015-01-21 17:39:11 -0700

    static contr meth to redirect if current user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,16 @@
 class StaticPagesController < ApplicationController
+  before_action :logged_in_redirect
+
   def home
+
   end
 
   def about
+  end
+
+  def logged_in_redirect
+    if current_user
+      redirect_to locations_url
+    end
   end
 end

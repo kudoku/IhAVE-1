efdbcbc0f30866e86567d5b7b2f8f7ceb3138f79
JPark <park.joonw@gmail.com>
2015-01-25 15:53:46 -0700

    quick and dirty bootstrap flash for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -39,6 +39,7 @@ class LocationsController < ApplicationController
 
   def update
     if @location.update_attributes(location_params)
+      flash[:notice] = "Location #{@location.name} updated."
       redirect_to locations_path #it would redirect to locations_show, instead of locations_path
     else
       render :edit
@@ -47,6 +48,7 @@ class LocationsController < ApplicationController
 
   def destroy
     @location.destroy
+    flash[:alert] = "Location #{@location.name} deleted."
     redirect_to locations_url
   end
 

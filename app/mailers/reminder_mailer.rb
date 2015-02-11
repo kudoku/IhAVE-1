10939bd46a137365fa7ab79b294618bc61de8656
JPark <park.joonw@gmail.com>
2015-02-11 10:15:52 -0700

    inc item name max length, item show styling, email styling

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,12 +1,12 @@
 class ReminderMailer < ActionMailer::Base
-  default from: "park.joonw@gmail.com"
+  default from: "ihavetest@gmail.com"
 
 
   def reminder_email_borrower(record)
 
     @record = record
     
-    mail(from: @record.item.user.email, to: @record.borrower_email, subject: "Friendly Reminder to Return #{@record.item.name} to #{@record.item.user.username}")
+    mail(to: @record.borrower_email, subject: "Friendly Reminder to Return #{@record.item.name} to #{@record.item.user.username}")
     
   end
 
@@ -15,6 +15,6 @@ class ReminderMailer < ActionMailer::Base
 
     @item = item
 
-    mail(from: @item.user.email, to: @item.user.email, subject: "Friendly Reminder to Return #{@item.name} to #{@item.borrowed_from}")
+    mail(to: @item.user.email, subject: "Friendly Reminder to Return #{@item.name} to #{@item.borrowed_from}")
   end
 end
